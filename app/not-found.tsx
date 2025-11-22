0ac0e0ba0459ef03b794cce8d6b80fc0c1d00d84
Marcelo Góes <marcelogoes@gmail.com>
2025-11-22 18:32:56 -0300

    update

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,6 @@
+"use client";
+import React from "react";
+
 export default function NotFound() {
     return (
         <div className="min-h-[70vh] flex flex-col items-center justify-center text-white">
